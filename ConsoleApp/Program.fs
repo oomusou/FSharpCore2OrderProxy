2030e23ee7f7296f2aed173c8a53402f8f621735
Oomusou <oomusou@gmail.com>
2018-04-10 15:45:21 +0800

    FP Proxy Pattern

--- ConsoleApp/Program.fs
+++ ConsoleApp/Program.fs
@@ -8,8 +8,15 @@ let main argv =
     let originalPrice = 800.0
     let discount = 0.8
     
+    let isMember = MemberService.isMember account
+    
+    let orderProxy =
+        match isMember with
+        | true  -> OrderService.getPrice
+        | false -> fun _ price -> price 
+    
     originalPrice
-    |> OrderService.getPrice discount
-    |> printfn "Real price:%0.0f"        
+    |> orderProxy discount
+    |> printf "Real price : %0.0f"        
 
     0 // return an integer exit code
---
+++ MemberLibrary/MemberService.fs
@@ -0,0 +1,4 @@
+namespace MemberLibrary
+
+module MemberService =
+    let isMember account = account = "Sam"
